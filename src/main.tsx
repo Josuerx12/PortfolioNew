ad539e51f22476ad460ae4a26ba004e5c075147f
josuerx12 <josueaze12@gmail.com>
2024-04-20 14:22:28 -0300

    feat: new features

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
