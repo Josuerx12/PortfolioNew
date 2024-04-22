ba5ad56780c159dfbc7cb8ab92c36a77fd44939a
Josue Silva de Azevedo de Carvalho <josueaze12@gmail.com>
2024-04-22 08:42:08 -0300

    feat: navbar update.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,7 +5,7 @@ import { handleScrollTo } from "../../hooks/useScroll";
 const Navbar = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
 
-  const list = ["home", "about", "portfolio", "contact", "experience"];
+  const list = ["home", "about", "portfolio", "experience", "contact"];
 
   return (
     <>
